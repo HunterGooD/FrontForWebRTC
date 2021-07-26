70260f701eb028d0d044ea89f0af10bb054de3e5
Vlad Gudov <vlad.gudov@inbox.ru>
2021-07-26 13:21:46 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+   
+  </v-app>
+</template>
+
+<script>
+import UserVideo from './components/UserVideo';
+
+export default {
+  name: 'App',
+
+  components: {
+    UserVideo,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/UserVideo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h2 v-if="source == null">
+      Нет потокового видео
+    </h2>
+    <div v-else>
+      <h4>{{ name }}</h4>
+      <video ref="video" autoplay :muted="{mute}" :controls="{control}" ></video>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'UserVideo',
+    props: [ "name", "src", "mute","control",],
+    data: () => ({
+      source: null
+    }),
+    methods: {
+      async loadSource(val) {
+        console.log(val);
+        let v = this;
+        v.source = val;
+        setTimeout(()=>{
+          v.$refs.video.srcObject = val;
+          clearTimeout(this);
+        }, 400);
+      }
+    },
+    created() {
+      if (this.src != null) {
+        this.loadSource(this.src)
+      }
+    },
+    watch: {
+      src: {
+         handler: function(val) {
+            this.loadSource(val); 
+        },
+        deep: true,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+video {
+  width:320px;
+  height:240px;
+}
+</style>
